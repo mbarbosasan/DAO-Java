8bbfbcdfbde102b1bb4c3a7c680368f59bfc5de6
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-04 10:39:19 -0300

    add: Seller class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,11 +1,17 @@
 package Application;
 
 import Model.Entities.Department;
+import Model.Entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bobo@gmail.com", new Date(),
+                3000.00, obj);
 
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
