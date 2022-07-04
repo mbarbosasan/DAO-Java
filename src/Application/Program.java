638acc34d763305710972449de62ef26f4f472ac
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-04 11:23:37 -0300

    add: Find seller by department

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -6,6 +6,7 @@ import Model.Entities.Department;
 import Model.Entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,5 +14,13 @@ public class Program {
         System.out.println("=== TEST 1: Seller find By Id ===");
         Seller seller = sellerDao.findById(2);
         System.out.println(seller);
+
+        System.out.println("=== TEST 2: Seller find by Department");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller item: list) {
+            System.out.println(item);
+        }
     }
 }
